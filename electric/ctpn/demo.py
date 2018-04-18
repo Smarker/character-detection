28559a8dc29a4b54d93940a1e49819d82280a1ce
Stephanie Marker <Stephanie.Marker93@gmail.com>
2018-04-18 12:27:44 +0200

    Add simon's crop changes.

--- electric/ctpn/demo.py
+++ electric/ctpn/demo.py
@@ -13,6 +13,7 @@ from lib.fast_rcnn.test import test_ctpn
 from lib.utils.timer import Timer
 from lib.text_connector.detectors import TextDetector
 from lib.text_connector.text_connect_cfg import Config as TextLineCfg
+from PIL import Image
 
 results_dir = os.path.join(os.path.pardir, '..', 'data', 'results')
 
@@ -43,9 +44,10 @@ def draw_boxes(img,image_name,boxes,scale):
             min_y = min(int(box[1]/scale),int(box[3]/scale),int(box[5]/scale),int(box[7]/scale))
             max_x = max(int(box[0]/scale),int(box[2]/scale),int(box[4]/scale),int(box[6]/scale))
             max_y = max(int(box[1]/scale),int(box[3]/scale),int(box[5]/scale),int(box[7]/scale))
-
+            
             line = ','.join([str(min_x),str(min_y),str(max_x),str(max_y)])+'\r\n'
             f.write(line)
+            crop_function(image_name,min_y,min_x,max_y,max_x)
 
     img=cv2.resize(img, None, None, fx=1.0/scale, fy=1.0/scale, interpolation=cv2.INTER_LINEAR)
     cv2.imwrite(os.path.join(results_dir, base_name), img)
@@ -53,7 +55,6 @@ def draw_boxes(img,image_name,boxes,scale):
 def ctpn(sess, net, image_name):
     timer = Timer()
     timer.tic()
-
     img = cv2.imread(image_name)
     img, scale = resize_im(img, scale=TextLineCfg.SCALE, max_scale=TextLineCfg.MAX_SCALE)
     scores, boxes = test_ctpn(sess, net, img)
@@ -65,6 +66,34 @@ def ctpn(sess, net, image_name):
     print(('Detection took {:.3f}s for '
            '{:d} object proposals').format(timer.total_time, boxes.shape[0]))
 
+
+def crop_function(image,left,top,right,bottom):
+    image_url=image
+    img=Image.open(image_url)
+    size=img.size
+    box=(left,size[1]-bottom,right,size[1]-top)
+    crop_area=img.crop(box)
+    indice=0
+
+    x=image_url.split(".")
+    s=""
+    y=x[0].split("/")
+    l=len(y)
+    i=0
+    for i in range(l):
+        if (i==l-1):
+            s=s+"results/"+y[i]
+        else:
+            s=s+y[i]+"/"
+    test=True
+    while test:
+        crop_url=s+"_crop_"+str(indice)+"."+x[1]
+        try:
+            with open(crop_url): pass
+        except IOError:
+            test=False
+        indice+=1
+    crop_area.save(crop_url)
 
 
 if __name__ == '__main__':
